adcfcb5b7e19de44b92cb9de2d87dc150c88ddbd
capolimia <capolimia@gmail.com>
2020-09-23 16:58:46 -0400

    Adding home page buttons w no functionality

--- Angular-AMEALGOES/src/app/app.module.ts
+++ Angular-AMEALGOES/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
