aedb683dfec9de66d4e5ac8962d360cac94ceda8
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-09-23 22:36:51 -0400

    Created template for 'Create-Meal' page

---
+++ Angular-AMEALGOES/src/app/create-meal/create-meal.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-meal',
+  templateUrl: './create-meal.component.html',
+  styleUrls: ['./create-meal.component.css']
+})
+export class CreateMealComponent implements OnInit {
+
+  newMealForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { 
+
+  }
+
+  ngOnInit(): void {
+
+    this.newMealForm = this.formBuilder.group({
+      zip: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(5), Validators.pattern("[0-9]")]],
+    });
+
+  }
+
+  get formFields() {
+    return this.newMealForm.controls;
+  }
+
+  beginMeal(){
+    this.submitted = true;
+
+  }
+
+}
