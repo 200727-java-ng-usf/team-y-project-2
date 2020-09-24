aff325809de47c34d74625c9f1d4301039471039
capolimia <capolimia@gmail.com>
2020-09-24 18:59:34 -0400

    fixing merge reqs

--- Angular-AMEALGOES/src/app/app-routing.module.ts
+++ Angular-AMEALGOES/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
+import { CreateMealComponent } from './create-meal/create-meal.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -11,19 +11,10 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'createmeal', component: RegisterComponent },
   { path: 'joinmeal', component: RegisterComponent },
-  { path: '', component: LoginComponent }
-=======
-import { CreateMealComponent } from './create-meal/create-meal.component';
+  { path: '', component: LoginComponent },
+  { path: 'newMeal', component: CreateMealComponent}
 
-const routes: Routes = [
-  { path: 'register', component: RegisterComponent }, // goes to register page
-  { path: 'login', component: LoginComponent }, // for when user registers, they get redirected to login
-  { path: '', component: LoginComponent }, // opening page
-  { path: 'newMeal', component: CreateMealComponent} // page for creating new 'meal' instance
->>>>>>> bcecd534b283a9a48bf0665a5ab3dd688678680d
-];
-
-
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
