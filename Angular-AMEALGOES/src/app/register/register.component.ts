552f9e1a0e3c86896f781d9afca3af4a96434493
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-09-26 20:49:07 -0400

    git thinks I changed something because I commented, saved, then uncommented it

--- Angular-AMEALGOES/src/app/register/register.component.ts
+++ Angular-AMEALGOES/src/app/register/register.component.ts
@@ -60,24 +60,5 @@ export class RegisterComponent implements OnInit {
                         console.log('observable complete!')
                       }
                     );
-
-
-
-
-
-
-
-
-    // The below values are to demonstrate form data is properly collected and validated
-
-    // let email = this.formFields.email.value;
-    // let username = this.formFields.username.value;
-    // let password = this.formFields.password.value;
-
-    // console.log('These are the entered values:');
-    // console.log(`Email: ${ email }`);
-    // console.log(`Username: ${ username }`);
-    // console.log(`Password: ${ password }`);
   }
-
 }
