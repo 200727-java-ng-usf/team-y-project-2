ac923c6a397b90ecd425066e56495ef2e29781cc
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-09-27 02:46:10 -0400

    worked on connecting create-meal.compoent to google-maps-connection-service

--- Angular-AMEALGOES/src/app/create-meal/create-meal.component.ts
+++ Angular-AMEALGOES/src/app/create-meal/create-meal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MealService } from '../services/meal.service';
+import { GoogleMapsConnectionService } from '../services/google-maps-connection.service';
 
 @Component({
   selector: 'app-create-meal',
@@ -20,8 +21,13 @@ export class CreateMealComponent implements OnInit {
   ngOnInit(): void {
 
     this.newMealForm = this.formBuilder.group({
-      zip: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(5), Validators.pattern(/^[0-9]\d*$/)]],
+      
       city: [null, Validators.required]
+      //zip: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(5), Validators.pattern(/^[0-9]\d*$/)]],
+      //city: [null, Validators.required]
+      
+
+
     });
 
   }
@@ -36,6 +42,28 @@ export class CreateMealComponent implements OnInit {
 
     if (this.newMealForm.invalid) return;
 
+    //need to get the value within city component, not sure how without getElementById
+    //put new york temporarily
+    let city = 'new york';
+
+    let gmcs = new GoogleMapsConnectionService;
+
+    //replace 'new york' with 'philadelphia' to get the other option
+
+    gmcs.getRestaurants(city).then(
+      responce => {
+
+        //testing what responce we got
+        console.log('this is the name of the first restaurant in the list' + responce[0].name);
+        console.log('this is the name of the last restaurant in the list' + responce[19].name);
+
+        //do post request here to put restaurants in database IF they dont exist
+
+
+    }
+    );
+    
+    
     // Uncomment when the service for connecting to the api is finished.
     //this.mealService.beginMeal(this.formFields.zip.value)
       
