f958af77dc6fa0c464ca146b26ec6ed5a4aa5029
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-09-28 15:14:06 -0400

    fixed weird commented code and HEAD markings

--- Angular-AMEALGOES/src/app/create-meal/create-meal.component.ts
+++ Angular-AMEALGOES/src/app/create-meal/create-meal.component.ts
@@ -21,16 +21,9 @@ export class CreateMealComponent implements OnInit {
   ngOnInit(): void {
 
     this.newMealForm = this.formBuilder.group({
-<<<<<<< HEAD
 
       city: [null, Validators.required]
 
-=======
-      
-      city: [null, Validators.required]
-      //zip: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(5), Validators.pattern(/^[0-9]\d*$/)]],
-      //city: [null, Validators.required]
->>>>>>> 3dd16f68398b34682d54f20942532dcf8f3d5e00
     });
 
   }
@@ -39,10 +32,7 @@ export class CreateMealComponent implements OnInit {
     return this.newMealForm.controls;
   }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 3dd16f68398b34682d54f20942532dcf8f3d5e00
   beginMeal(){
 
     this.submitted = true;
@@ -54,7 +44,7 @@ export class CreateMealComponent implements OnInit {
 
     //need to get the value within city component, not sure how without getElementById
     //put new york temporarily
-    let city = 'new york';
+    let city = this.formFields.city.value;
 
     let gmcs = new GoogleMapsConnectionService;
 
@@ -66,23 +56,13 @@ export class CreateMealComponent implements OnInit {
         //testing what responce we got
         console.log('this is the name of the first restaurant in the list' + responce[0].name);
         console.log('this is the name of the last restaurant in the list' + responce[19].name);
-
         //do post request here to put restaurants in database IF they dont exist
+      
+      
+    });
 
-
-    }
-    );
-    
     // Uncomment when the service for connecting to the api is finished.
-    //this.mealService.beginMeal(this.formFields.zip.value)
-<<<<<<< HEAD
+    // this.mealService.beginMeal(this.formFields.zip.value)
   }
-=======
-  beginMeal(){ 
-
-    this.mealService.sendMeal(this.formFields.location.value)
-  
-}
->>>>>>> 3dd16f68398b34682d54f20942532dcf8f3d5e00
 
 }
