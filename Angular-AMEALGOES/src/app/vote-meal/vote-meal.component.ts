fffcd9f609613ad04e45bafa2702297d0e482694
capolimia <capolimia@gmail.com>
2020-09-30 14:52:24 -0400

    vote meal displays resturants in meal..........

--- Angular-AMEALGOES/src/app/vote-meal/vote-meal.component.ts
+++ Angular-AMEALGOES/src/app/vote-meal/vote-meal.component.ts
@@ -1,6 +1,10 @@
+import { ThrowStmt } from '@angular/compiler';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { meal } from '../models/meal';
+import { restaurant } from '../models/restaurant';
 import { MealService } from '../services/meal.service';
 
 @Component({
@@ -10,8 +14,11 @@ import { MealService } from '../services/meal.service';
 })
 export class VoteMealComponent implements OnInit{
 
-  currentResturant: number;
+  currentResturantInt: number;
   resturants: meal;
+  currentResturant: restaurant;
+
+  
 
   constructor(private mealService: MealService) { }
 
@@ -19,22 +26,36 @@ export class VoteMealComponent implements OnInit{
 
   async ngOnInit() {
     this.resturants = <meal> await this.mealService.getResturants();
-    this.currentResturant = 0;
-    
-    
+    this.currentResturantInt = 0;
+    this.currentResturant = this.resturants.restaurants[this.currentResturantInt];
+    console.log(this.currentResturant);
 
   }
   
 
-  getNextResturant() {
-    this.currentResturant++;
+  getNextResturantVote() {
+    this.currentResturantInt++;
     
-    if (this.resturants.restaurants[this.currentResturant] != null) {
-      return this.resturants.restaurants[this.currentResturant];
+
+
+    if (this.resturants.restaurants[this.currentResturantInt] != null) {
+      return this.currentResturant = this.resturants.restaurants[this.currentResturantInt];
     } else {
       //voting done?
     }
     
+  }
+
+  getNextResturantSkip() {
+    this.currentResturantInt++;
+    
+
+
+    if (this.resturants.restaurants[this.currentResturantInt] != null) {
+      return this.currentResturant = this.resturants.restaurants[this.currentResturantInt];
+    } else {
+      //voting done?
+    }
     
   }
 
